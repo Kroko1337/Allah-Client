a8646a832404403153181be828cf1f68d26f8afe
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2021-11-06 21:31:21 +0100

    added Toggle Command

---
+++ src/minecraft/main/kotlin/god/allah/commands/Toggle.kt
@@ -0,0 +1,26 @@
+package god.allah.commands
+
+import god.allah.api.Registry
+import god.allah.api.executors.Command
+import god.allah.api.executors.CommandInfo
+import god.allah.main.Wrapper.getModule
+import god.allah.main.Wrapper.sendMessage
+
+@CommandInfo("toggle", ["t"])
+class Toggle : Command() {
+    override fun execute(args: Array<String>): Boolean {
+        when(args.size) {
+            1 -> {
+                val module = getModule(args[0])
+                if(module != null) {
+                    module.toggle()
+                    sendMessage((if(module.isToggled()) "§a" else "§c") + "Toggled §e${module.name}")
+                } else {
+                    sendMessage("§e${args[0]} §cis not a valid Module!")
+                }
+                return true
+            }
+        }
+        return false
+    }
+}

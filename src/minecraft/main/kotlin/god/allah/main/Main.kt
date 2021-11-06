664efd5dad092bbf971a918871566bb8840c159e
Kroko1337 <z&tLDL9B6TXX9Dk^yLhVHyTeztb>
2021-11-06 15:52:20 +0100

    added Optifine

---
+++ src/minecraft/main/kotlin/god/allah/main/Main.kt
@@ -0,0 +1,19 @@
+package god.allah.main
+
+import god.allah.main.Wrapper.instance
+import god.allah.main.Wrapper.name
+import god.allah.main.Wrapper.version
+import org.lwjgl.opengl.Display
+
+class Main {
+
+    fun onStart() {
+        instance = this
+        Display.setTitle("$name $version | Minecraft 1.12")
+        Runtime.getRuntime().addShutdownHook(Thread() { onShutdown() })
+    }
+
+    private fun onShutdown() {
+
+    }
+}
---
+++ src/minecraft/main/kotlin/god/allah/main/Wrapper.kt
@@ -0,0 +1,7 @@
+package god.allah.main
+
+object Wrapper {
+    const val name = "Allah"
+    const val version = "v1.0"
+    lateinit var instance: Main
+}

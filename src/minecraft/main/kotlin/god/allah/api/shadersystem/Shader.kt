e906b6916bb05ed14afc42fb5c736298537a6c4c
FelixH2012 <felixhans2007@gmail.com>
2021-11-09 17:10:57 +0100

    current working on shader system. not working right now

---
+++ src/minecraft/main/kotlin/god/allah/api/shadersystem/Shader.kt
@@ -0,0 +1,42 @@
+package god.allah.api.shadersystem
+
+import net.minecraft.util.ResourceLocation
+import org.apache.commons.io.IOUtils
+import org.lwjgl.opengl.ARBFragmentShader
+import org.lwjgl.opengl.ARBShaderObjects
+import java.io.InputStream
+
+
+/**
+ *
+ * Coded by Felix1337 at 09.11.2021 started development : 16:53
+ *
+ */
+
+abstract class Shader(shaderName: String) {
+
+    private var programId = 0
+
+    private var resourceLocation: InputStream
+
+    init {
+        resourceLocation = javaClass.getResourceAsStream("/god/allph/api/shadersystem$shaderName")
+        this.programId = ARBShaderObjects.glCreateProgramObjectARB() //setting up shaderProgramID ad start.
+    }
+
+    //function to compile the shader: idk if it works
+    fun compileShader(type: ShaderType): Any {
+        val shaderObject = ARBShaderObjects.glCreateShaderObjectARB(type.SHADER_TYPE)
+        val fragID : Int = ShaderUtil.Companion.createShader(IOUtils.toString(resourceLocation), ARBFragmentShader.GL_FRAGMENT_SHADER_ARB)
+        IOUtils.closeQuietly(resourceLocation)
+        ARBShaderObjects.glCompileShaderARB(shaderObject)
+        if (ARBShaderObjects.glGetObjectParameteriARB(shaderObject, ARBShaderObjects.GL_OBJECT_COMPILE_STATUS_ARB) == 0) {
+            println("Unable to compile shader $shaderObject")
+        } else {
+            ARBShaderObjects.glAttachObjectARB(programId, shaderObject)
+            ARBShaderObjects.glAttachObjectARB(programId, fragID)
+
+        }
+        return this
+    }
+}
---
+++ src/minecraft/main/kotlin/god/allah/api/shadersystem/ShaderType.kt
@@ -0,0 +1,8 @@
+package god.allah.api.shadersystem
+
+import org.lwjgl.opengl.ARBFragmentShader
+import org.lwjgl.opengl.ARBVertexShader
+
+enum class ShaderType(val SHADER_TYPE: Int) {
+    VERTEX_SHADER(ARBVertexShader.GL_VERTEX_SHADER_ARB), FRAGMENT_SHADER(ARBFragmentShader.GL_FRAGMENT_SHADER_ARB);
+}
